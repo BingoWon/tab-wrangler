cd610501eb41263cb0ea35054eb60e01f30f32de
BingoWon <bingow@outlook.com>
2024-10-02 11:38:24 +0800

    Enhance tab management and incognito handling
    - Implement incognito-aware tab deduplication
    - Refactor for modern JavaScript practices
    - Improve error handling and code maintainability
    - Remove deprecated API usage and unused code

--- js/background.js
+++ js/background.js
@@ -1,10 +1,8 @@
 "use strict";
 
-var App = {
-
+const App = {
 	tabs: Object.create(null),
 	options: Object.create(null),
-
 	recentlyClosedTabUrl: undefined,
 
 	initialize: function (tabs) {
@@ -20,83 +18,49 @@ var App = {
 	},
 
 	updateOrCloseTab: function (tabId, changeInfo, tab) {
-		// console.log("tab:", tab);
-		// Bin: Sometimes the tab is already closed.
 		if (this.tabs[tabId] === undefined) return;
 
-		if (!tab.url || this.tabs[tabId].kicked || tab.url.indexOf('chrome://') === 0 || this.tabs[tabId].preventFromClosing) {
+		if (!tab.url || this.tabs[tabId].kicked || tab.url.startsWith('chrome://') || this.tabs[tabId].preventFromClosing) {
 			return;
 		}
 
-		// pendingUrl available only from Chrome v79
-		// Bin: I want pendingUrl but I have to make sure everything before it exist.
-		let pendingUrl = this.tabs[tabId] && this.tabs[tabId].tab && this.tabs[tabId].tab.pendingUrl;
-		// Bin: We don't care if they are in the same window or not. windowId won't be used.
-		var existedTabs = this.getTabsByUrlInSameWindow(pendingUrl || tab.url, tab.windowId);
+		const pendingUrl = this.tabs[tabId]?.tab?.pendingUrl;
 
-		var anotherTabs = existedTabs.filter(function (existedTab) {
-			return existedTab.id !== tab.id;
+		// Check if the tab is in incognito mode
+		chrome.tabs.get(tabId, (tabInfo) => {
+			if (chrome.runtime.lastError) {
+				console.error(chrome.runtime.lastError);
+				return;
+			}
+
+			const existedTabs = this.getTabsByUrlInSameWindow(pendingUrl || tab.url, tabInfo.incognito);
+
+			const anotherTabs = existedTabs.filter(existedTab => existedTab.id !== tab.id);
+
+			if (anotherTabs.length) {
+				this.tabs[tabId].kicked = true;
+				this.recentlyClosedTabUrl = tab.url;
+				this.highlightTab(anotherTabs[0].id, {
+					kickedUrl: tab.url,
+					kickedTabIndex: tab.index,
+					kickedTabWindowId: tab.windowId,
+					incognito: tabInfo.incognito
+				});
+				this.closeTab(tabId);
+			}
+
+			this.tabs[tabId].tab = tab;
 		});
-
-		if (anotherTabs.length) {
-			this.tabs[tabId].kicked = true;
-			// chrome.scripting.executeScript({
-			// 	target: { tabId: tabId },
-			// 	func: () => {
-			// 		window.stop();
-			// 	},
-			// 	world: "MAIN"
-			// })
-			this.recentlyClosedTabUrl = tab.url;
-			this.highlightTab(anotherTabs[0].id, {
-				kickedUrl: tab.url,
-				kickedTabIndex: tab.index,
-				kickedTabWindowId: tab.windowId
-			});
-			this.closeTab(tabId);
-		}
-		// if (anotherTabs.length) {
-		// 	this.tabs[tabId].kicked = true;
-		// 	// The line below prevents the tab from invoking the same update event the second time.
-		// 	// `chrome.scripting.executeScript` returns a promise.
-		// 	chrome.scripting.executeScript({
-		// 		target: { tabId: tabId },
-		// 		func: () => {
-		// 			window.stop();
-		// 		},
-		// 		world: "MAIN"
-		// 	}).then(() => {
-		// 		this.recentlyClosedTabUrl = tab.url;
-		// 		this.highlightTab(anotherTabs[0].id, {
-		// 			kickedUrl: tab.url,
-		// 			kickedTabIndex: tab.index,
-		// 			kickedTabWindowId: tab.windowId
-		// 		});
-		// 		this.closeTab(tabId);
-		// 	});
-		// }
-
-		this.tabs[tabId].tab = tab;
 	},
 
 	listenReplaceTab: function (newTabId, oldTabId) {
 		this.tabs[newTabId] = this.tabs[oldTabId];
 		this.removeTabWatching(oldTabId);
 
-		chrome.tabs.get(newTabId, function (tab) {
+		chrome.tabs.get(newTabId, tab => {
 			this.tabs[newTabId].tab = tab;
 			this.updateOrCloseTab(newTabId, {}, tab);
-		}.bind(this));
-	},
-
-	listenBeforeRequest: function (details) {
-		if (details.tabId && details.tabId > 0 && this.tabs[details.tabId] && this.tabs[details.tabId].kicked) {
-			return {
-				cancel: true
-			};
-		}
-
-		return {};
+		});
 	},
 
 	addTab: function (tab) {
@@ -111,59 +75,39 @@ var App = {
 	},
 
 	highlightTab: function (tabId, options) {
-		const { kickedUrl, kickedTabIndex, kickedTabWindowId } = options;
+		const { kickedUrl, kickedTabIndex, kickedTabWindowId, incognito } = options;
 		const { replace_hash_for_old_tab, move_tab } = this.options;
 
-		// Bin: bring the kept duplicate tab to the closed duplicate tab (where the new tab is opened).
-		// In Edge, there can be more than one "profile" which is different user space.
-		// Tabs can only be moved between windows in the same profile.
-		// But I can find any properties related to `profile` in tab object.
-		// It will always throw an error if the tabs are not in the same profile.
-		try {
-			if (move_tab) {
-				chrome.tabs.move(tabId, {
-					index: kickedTabIndex,
-					windowId: kickedTabWindowId
-				});
-			}
-		} catch (e) {
-			console.error("Moving tab Failed: ", e);
+		// Only move the tab if it's in the same incognito state
+		if (move_tab) {
+			chrome.tabs.get(tabId, (tab) => {
+				if (tab.incognito === incognito) {
+					chrome.tabs.move(tabId, {
+						index: kickedTabIndex,
+						windowId: kickedTabWindowId
+					}).catch(e => console.error("Moving tab Failed: ", e));
+				}
+			});
 		}
-		// bring the window where the new tab is opened to the front.
-		chrome.windows.update(kickedTabWindowId, {
-			focused: true
-		});
 
-		// bring the kept duplicate tab to the front.
-		chrome.tabs.update(tabId, {
-			highlighted: true,
-			active: true
-		});
+		chrome.windows.update(kickedTabWindowId, { focused: true });
+		chrome.tabs.update(tabId, { highlighted: true, active: true });
 
-
-		// if (replace_hash_for_old_tab && kickedUrl) {
-		// 	const url = new URL(kickedUrl);
-		// 	if (url.hash) {
-		// 		chrome.scripting.executeScript({
-		// 			target: { tabId: tabId },
-		// 			func: setHash,
-		// 			args: [url.hash]
-		// 		});
-		// 	}
-		// }
-
-		// function setHash(hash) {
-		// 	location.hash = hash;
-		// }
+		if (replace_hash_for_old_tab && kickedUrl) {
+			const url = new URL(kickedUrl);
+			if (url.hash) {
+				chrome.tabs.sendMessage(tabId, { action: "setHash", hash: url.hash });
+			}
+		}
 	},
 
 	closeTab: function (tabId) {
-		chrome.tabs.remove(tabId, function () {
+		chrome.tabs.remove(tabId).then(() => {
 			this.removeTabWatching(tabId);
-		}.bind(this));
+		}).catch(e => console.error("Closing tab Failed: ", e));
 	},
 
-	urlMatch(url1, url2) {
+	urlMatch: function (url1, url2) {
 		const { ignore_hash } = this.options;
 
 		if (ignore_hash) {
@@ -174,18 +118,14 @@ var App = {
 		return url1 === url2;
 	},
 
-	getTabsByUrlInSameWindow: function (url, windowId) {
-		var result = [];
-		for (var tabId in this.tabs) {
-			var tab = this.tabs[tabId];
-
-			// Bin: We don't care if they are in the same window or not.
-			// if (this.urlMatch(tab.tab.url, url) && !tab.kicked && tab.tab.windowId === windowId) {
-			if (this.urlMatch(tab.tab.url, url) && !tab.kicked) {
-				result.push(this.tabs[tabId].tab);
-			}
-		}
-		return result;
+	getTabsByUrlInSameWindow: function (url, isIncognito) {
+		return Object.values(this.tabs)
+			.filter(tab =>
+				this.urlMatch(tab.tab.url, url) &&
+				!tab.kicked &&
+				tab.tab.incognito === isIncognito
+			)
+			.map(tab => tab.tab);
 	}
 };
 
@@ -200,33 +140,4 @@ chrome.tabs.onUpdated.addListener(App.updateOrCloseTab.bind(App));
 chrome.tabs.onRemoved.addListener(App.removeTabWatching.bind(App));
 chrome.tabs.onReplaced.addListener(App.listenReplaceTab.bind(App));
 
-// chrome.webRequest.onBeforeRequest.addListener(App.listenBeforeRequest.bind(App),
-// 	{
-// 		urls: ["http://*/*", "https://*/*"],
-// 		types: ["sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
-// 	},
-// 	["blocking"]
-// );
-
-// const rule = {
-// 	id: tabId,
-// 	priority: 1,
-// 	action: { type: "block" },
-// 	condition: {
-// 		// tabIds: [tabId],
-// 		urlFilter: "",	// placeholder for now
-// 		resourceTypes: ["sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
-// 	}
-// };
-
-// async function getAllSessionRuleIds() {
-// 	const oldRules = await chrome.declarativeNetRequest.getDynamicRules();
-// 	const oldRuleIds = oldRules.map(rule => rule.id);
-// 	return oldRuleIds
-// }
-
-// // Use the arrays to update the dynamic rules
-// await chrome.declarativeNetRequest.updateSessionRules({
-// 	removeRuleIds: oldRuleIds,
-// 	addRules: newRules
-// });
+// Implement declarativeNetRequest rules here if needed
